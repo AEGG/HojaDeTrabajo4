fd1fbdee84359e1823fc31fc1ae49085978e782e
javobu94 <javobu_94@hotmail.com>
2014-08-04 14:57:05 -0600

    Cambio 1.0.2

--- calculadora/src/calculadora/StackArrayList.java
+++ calculadora/src/calculadora/StackArrayList.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 public class StackArrayList<E> extends AbstractStack<E>
 {
 	protected ArrayList<E> data;
+        protected int cont;
 
 	public StackArrayList()
 	// post: constructs a new, empty stack
@@ -24,32 +25,33 @@ public class StackArrayList<E> extends AbstractStack<E>
 	//          will be popped next if no intervening push
 	{
 		data.add(item);
+                cont++;
 	}
 
 	public E pop()
 	// pre: stack is not empty
 	// post: most recently pushed item is removed and returned
 	{
-		return data.remove(size()-1);
+		return data.remove(cont-1);
 	}
 
 	public E peek()
 	// pre: stack is not empty
 	// post: top value (next to be popped) is returned
 	{
-		return data.get(size() - 1);
+		return data.get(cont-1);
 	}
 	
 	public int size()
 	// post: returns the number of elements in the stack
 	{
-		return data.size();
+		return cont;
 	}
   
 	public boolean empty()
 	// post: returns true if and only if the stack is empty
 	{
-           return size() == 0; 
+           return cont == 0; 
         }
 }
     
