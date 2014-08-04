81db28e5ff7d9cbb9e58af7ef5eec80b07d6b6ce
AEGG <aegiron@hotmail.com>
2014-08-04 17:26:31 -0600

    Cambio 1.0.4

---
+++ calculadora/src/calculadora/List.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package calculadora;
+
+/**
+ *
+ * @author Acer
+ */
+public interface List<E> 
+{
+
+   public int size();
+   // post: returns number of elements in list
+
+   public boolean isEmpty();
+   // post: returns true iff list has no elements
+
+   public void clear();
+   // post: empties list
+
+   public void addFirst(E value);
+   // post: value is added to beginning of list
+
+  
+   public E getFirst();
+   // pre: list is not empty
+   // post: returns first value in list
+
+   public E removeFirst();
+}
+
